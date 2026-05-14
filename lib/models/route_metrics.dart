52b62ccaaa26d1bbbadc6bc90f11872bee024e05
ph4nt0m37x <marija.ilievskax@gmail.com>
2026-05-14 21:46:28 +0200

    Formatted better

--- lib/models/route_metrics.dart
+++ lib/models/route_metrics.dart
@@ -10,7 +10,12 @@ class RouteMetrics {
   // computed getters for formatted values
   double get distanceKm => distanceMeters / 1000;
 
-  String get formattedDistance => '${distanceKm.toStringAsFixed(1)} km';
+  String get formattedDistance {
+    if (distanceMeters < 1000) {
+      return '${distanceMeters.toStringAsFixed(0)} m';
+    }
+    return '${distanceKm.toStringAsFixed(1)} km';
+  }
 
   Duration get duration => Duration(seconds: durationSeconds);
 
@@ -21,7 +26,10 @@ class RouteMetrics {
     if (hours > 0) {
       return '$hours hr $minutes min';
     }
-    return '$minutes min';
+    if (minutes > 0) {
+      return '$minutes min';
+    }
+    return '$durationSeconds sec';
   }
 
   factory RouteMetrics.fromJson(Map<String, dynamic> json) {
